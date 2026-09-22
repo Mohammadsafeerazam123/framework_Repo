556efe70a74877d9c9d120deddb78140daab121b
user <user@DESKTOP-HRE0M34>
2026-09-22 10:17:43 +0530

    Commit

---
+++ seleniumproject/src/test/java/package1/ContactTest.java
@@ -0,0 +1,27 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("Executed create Contact Test");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("Executed modify Contact Test");
+		
+		// Repo url https://github.com/Mohammadsafeerazam123/framework_Repo.git
+	}
+
+}
---
+++ seleniumproject/src/test/java/package2/OrgTest.java
@@ -0,0 +1,17 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrg() {
+		System.out.println("Executed createOrg Test");
+	}
+	
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println("Executed modifyOrg Test");
+	}
+}
